c4fe0b17a16a6e9b5c1e44f3b95ef3976943757f
Antonio-pf <antoniopfelipe03@gmail.com>
2022-01-08 13:12:20 -0300

    test

--- ex004.py
+++ ex004.py
@@ -1,1 +1,13 @@
-#
+#Discrições da variável
+algo = input('Ditite algo: \n')
+print('\n-----------Dissecando------------\n')
+print('Você digitou: {}'.format(algo))
+print('Variável do tipo: ',type(algo))
+print('É númerica?--->{}'.format(algo.isnumeric()))
+print('É alfa númerica?--->{}'.format(algo.isalnum()))
+print('É alfabético?--->{}'.format(algo.isalpha()))
+print('É minúscula?--->{}'.format(algo.islower()))
+print('É maiúscula?--->{}'.format(algo.isascii()))
+print('É decimal?--->{}'.format(algo.isdecimal()))
+print('Tem espaço?--->{}'.format(algo.isspace()))
+
