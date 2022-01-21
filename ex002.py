d6d18c2a729de9b20d2cffe072d49a4ec621dff1
Antonio-pf <antoniopfelipe03@gmail.com>
2022-01-21 00:03:37 -0300

    Formatter

--- ex002.py
+++ ex002.py
@@ -2,3 +2,12 @@ name = input('Digite seu nome: ')
 print('Prazer', name)
 #outro modo para formatar e dar espaços etc.
 print('Prazer {}!'.format(name))
+#arredondando com format
+number = 123.32
+number2 = 265.4963
+print(number * number2)
+print('<---Arredondamento para duas casa--->')
+result = number * number2
+print('{:.2f}'.format(result))
+print('{:.3f}'.format(result))
+print('{:.4f}'.format(result))
