ec99a0fb6fef2ce5ba73adf3d5dc8ba6e5c9ae64
Antonio-pf <antoniopfelipe03@gmail.com>
2022-01-21 00:12:29 -0300

    Operadores aritméticos

--- ex006.py
+++ ex006.py
@@ -1,4 +1,4 @@
 number = int(input('Digite um número: '))
 print('O dobro de {} vale: {}'.format(number,(number*2)))
 print('O triplo de {} vale: {}'.format(number, (number*3)))
-print('A raiz quadrada de: {} vale {}'.format(number, (number**(1/2))))
+print('A raiz quadrada de: {} vale {:.2f}'.format(number, (number**(1/2))))
