83f774556ef4e04fa2b5d13762642070bf52cacc
Antonio-pf <antoniopfelipe03@gmail.com>
2022-01-21 00:07:08 -0300

    Operadores aritméticos

---
+++ ex005.py
@@ -0,0 +1,6 @@
+number = int(input('Digite um número: '))
+antecessor = number - 1
+sucessor = number + 1
+print('Analisando o valor {}, seu antecessor é: {} e o sucessor é: {}'.format(number,(number-1),(number+2)))
+print('Analisando o valor {} seu antecessor é: {} e o sucessor é: {}'.format(number, antecessor, sucessor ))
+print(type(1))
---
+++ ex006.py
@@ -0,0 +1,4 @@
+number = int(input('Digite um número: '))
+print('O dobro de {} vale: {}'.format(number,(number*2)))
+print('O triplo de {} vale: {}'.format(number, (number*3)))
+print('A raiz quadrada de: {} vale {}'.format(number, (number**2)))
