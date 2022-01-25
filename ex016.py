d6b31fcd73e81e3484261597d631466dc19fe4b9
Antonio-pf <antoniopfelipe03@gmail.com>
2022-01-25 20:55:33 -0300

    Utilizando módulos

---
+++ ex016.py
@@ -0,0 +1,9 @@
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O número {} tem a parte inteira igual: {}'. format(num, trunc(num)))
+
+'''outra maneira simples'''
+
+num = float(input('Digite um valor: '))
+print('O número {} tem a parte inteira igual: {}'. format(num, int(num)))
+
---
+++ ex017.py
@@ -0,0 +1,8 @@
+##Hipotenusa
+from math import hypot
+cat_oposto = float(input('Digite o cateto oposto: '))
+cat_adjacente = float(input('Digite o cateto adjacente: '))
+hip = hypot(cat_oposto, cat_adjacente)
+
+print('Comprimento da hipotenusa: {:.2f}'.format(hip))
+
