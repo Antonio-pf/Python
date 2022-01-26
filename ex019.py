03353394e445ba4d9d56b4d566278d070ca004ac
Antonio-pf <antoniopfelipe03@gmail.com>
2022-01-25 21:35:04 -0300

    Utilizando Módulos

---
+++ ex019.py
@@ -0,0 +1,17 @@
+# Sorteio
+import random
+
+nome_alunos = []
+
+name = input('Insira o nome do aluno: ')
+nome_alunos.append(name)
+
+while name:
+    name = input('Insira o nome do aluno: ')
+    nome_alunos.append(name)
+    print(nome_alunos[:])
+
+
+print('----------------------------------')
+escolhido = random.choice(nome_alunos)
+print('O escolhido foi: ', escolhido)
