f1ac6697203cefffb8fddb70390ce5b83873c354
Antonio-pf <antoniopfelipe03@gmail.com>
2022-01-27 22:12:48 -0300

    Separando dígitos

--- mundo_1/aula_9/ex023.py
+++ mundo_1/aula_9/ex023.py
@@ -1,11 +1,17 @@
 # desafio
-num = str(input('Digite um valor de 0-9999: '))
+num = int(input('informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num //  100 % 10
+m = num // 1000 % 10
 
 print()
 print('Quantidade de:')
 print('------------')
-print('Unidades:{}'.format(num[:3]))
-print('Dezenas: {}'.format(num[:2]))
-print('Centenas: {}'.format(num[:1]))
-print('Milhares: {}'.format(num[:0]))
+print('Unidades:{}'.format(u))
+print('Dezenas: {}'.format(d))
+print('Centenas: {}'.format(c))
+print('Milhares: {}'.format(m))
 print('------------')
+
+
